358e469d0d4c43f20d430560de9b1d20db33c76b
BorisMargarian <margarian.boris@gmail.com>
2019-05-08 12:13:44 -0400

    added react-router and auto-generated house routes and controller

---
+++ app/javascript/react/containers/HousesContainer.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react"
+import { Route, IndexRoute, Router, browserHistory } from "react-router";
+
+class HousesContainer extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+  render() {
+    debugger
+    return (
+      <div>
+        <p>House component</p>
+      </div>
+    )
+  }
+}
+
+export default HousesContainer
