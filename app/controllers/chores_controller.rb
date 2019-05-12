002567a339ad442a8d7385596579ec28de73a99f
BorisMargarian <margarian.boris@gmail.com>
2019-05-12 10:59:04 -0400

    figured out a way to seed chore photos. Users can now add chores

--- app/controllers/chores_controller.rb
+++ app/controllers/chores_controller.rb
@@ -1,5 +1,23 @@
 class ChoresController < ApplicationController
   def new
-    binding.pry
+    @chore = Chore.new
   end
+
+  def create
+    @chore = Chore.new(chore_params)
+    @chore.house = current_user.house
+    if @chore.save
+      redirect_to house_path(current_user.house)
+      flash[:notice] = "Your chore has been added successfully!"
+    else
+      flash.now[:notice] =
+      @chore.errors.full_messages.join(', ')
+      render :new
+    end
+  end
+
+  private
+    def chore_params
+      params.require(:chore).permit(:name, :description, :cost, :due, :chore_photo)
+    end
 end
--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,6 +1,5 @@
 class HomesController < ApplicationController
   skip_before_action :authenticate_user!, :only => [:index]
   def index
-    @chore = Chore.new
   end
 end
