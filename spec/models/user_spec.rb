64f453fec5ed4647cdcc953da99dda70eb39a340
BorisMargarian <margarian.boris@gmail.com>
2019-06-07 13:47:18 -0400

    refined my user model and tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
+  let!(:test_user) {User.create(username: 'Test User', email: 'test.user@gmail.com', password: 'test-user')}
+  let!(:failed_user) {User.create()}
+  let(:errors) {failed_user.errors.full_messages}
+
+  scenario 'test_user is created successfully' do
+    expect(test_user.username).to eq('Test User')
+    expect(test_user.email).to eq('test.user@gmail.com')
+    expect(test_user.password).to eq('test-user')
+  end
+
+  scenario 'blank user fails to be created' do
+    expect(errors.length).to be 3
+    expect(errors).to include('Username can\'t be blank')
+    expect(errors).to include('Email can\'t be blank')
+    expect(errors).to include('Password can\'t be blank')
+  end
+
+  scenario 'two users cannot have the same: username or email' do
+    failed_user.update_attributes(username: 'Test User', email: 'test.user@gmail.com')
+    expect(errors.length).to be 3
+    expect(errors).to include('Username has already been taken')
+    expect(errors).to include('Email has already been taken')
+  end
 end
