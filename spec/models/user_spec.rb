7795438f1f604deb3d86548b811e53d5e68aa87d
BorisMargarian <margarian.boris@gmail.com>
2019-06-09 12:37:38 -0400

    added association tests to the user model for house and chores

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,10 @@ RSpec.describe User, type: :model do
   let!(:test_user) {User.create(username: 'Test User', email: 'test.user@gmail.com', password: 'test-user')}
   let!(:failed_user) {User.create()}
   let(:errors) {failed_user.errors.full_messages}
+  let(:test_house) {House.create(name: 'Test House', password: 'test-house')}
+  let(:test_house1) {House.create(name: 'Test House1', password: 'test-house1')}
+  let(:test_chore) {Chore.create(name: 'Test Chore', house: test_house)}
+  let(:test_chore1) {Chore.create(name: 'Test Chore1', house: test_house)}
 
   scenario 'test_user is created successfully' do
     expect(test_user.username).to eq('Test User')
@@ -23,5 +27,25 @@ RSpec.describe User, type: :model do
     expect(errors.length).to be 3
     expect(errors).to include('Username has already been taken')
     expect(errors).to include('Email has already been taken')
+  end
+
+  scenario 'newly registered user does not belong to a house' do
+    expect(test_user.house.nil?).to be true
+  end
+
+  scenario 'a user can belong to only a single house' do
+    test_user.house = test_house
+    expect(test_user.house).to be test_house
+    test_house1.users << test_user
+    expect(test_user.house).to be test_house1
+  end
+
+  scenario 'newly registered user should not have any chores' do
+    expect(test_user.chores.empty?).to be true
+  end
+
+  scenario 'a user can have many chores' do
+    test_user.chores << test_chore << test_chore1
+    expect(test_user.chores.size).to eq 2
   end
 end
